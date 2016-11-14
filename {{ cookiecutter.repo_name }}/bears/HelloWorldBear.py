1ab95e00971154b0cbaa04a7576ba1c09c93356a
Sourav Singh <souravsingh@users.noreply.github.com>
2016-11-14 20:22:49 +0530

    Update HelloWorldBear.py

--- {{ cookiecutter.repo_name }}/bears/HelloWorldBear.py
+++ {{ cookiecutter.repo_name }}/bears/HelloWorldBear.py
@@ -1,7 +1,16 @@
-from coalib.bears.LocalBear import LocalBear
+from coalib.bears.LocalBear import Loc
 
 
-class HelloWorldBear(LocalBear):
+class HelloWorldBear:
+    """
+    Add description for you bear here!
+    """
+    LANGUAGES = {"Python", "Python 2", "Python 3"}
+    AUTHORS = {full_name}
+    REQUIREMENTS = {PipRequirement('mypy-lang', '0.*')}
+    AUTHORS_EMAILS = {email}
+    LICENSE = license
+    ASCIINEMA_URL = 'https://asciinema.org/a/90736'
 
     def run(self,
             filename,
